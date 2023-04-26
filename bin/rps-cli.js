5501b1552f96e133d65a9354c8902986c99fc9fe
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-26 13:57:31 -0400

    made changes on rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
-import { rps } from "node-rpsls";
-
+import { rps } from "../lib/rpsls.js";
 const args = minimist(process.argv.slice(2));
-
 if (args.h || args.help) {
     displayHelp();
     process.exit(0);
